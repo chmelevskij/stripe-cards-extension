804296e547717b06fe4bd1637986fb675fb7c61b
Tom Chmelevskij <Tom.Chmelevskij@tido-music.com>
2018-12-20 13:38:00 +0100

    feat: more cards

--- js/browser_action.js
+++ js/browser_action.js
@@ -1,2 +1,11 @@
 
-console.log('one day this will be dynamic')
+window.onload = async () => {
+  console.log('one day this will be dynamic')
+  console.log(document.querySelectorAll('.card-number'));
+  try {
+    await navigator.clipboard.writeText('mememememe');
+    console.log('written to clipboard');
+  } catch(error) {
+    console.error(error);
+  }
+}
